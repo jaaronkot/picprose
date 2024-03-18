24e2d9f7fd8b0fb1f7a2862dc63cf92050677faa
gezhaoyou <zhaoyouge@gmail.com>
2024-03-18 12:01:28 +0800

    图片圆角OK

--- src/app/RightListboxWrapper.tsx
+++ src/app/RightListboxWrapper.tsx
@@ -1,70 +1,117 @@
 'use client'
 import React from "react";
-import {Listbox, ListboxItem, Chip, ScrollShadow, Avatar, Image, Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
-import {users} from "./data";
-import {AcmeLogo} from "./AcmeLogo";
+import { Tabs, Tab, Card, Listbox, CardBody, ListboxItem, Chip, ScrollShadow, Avatar, Image, Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from "@nextui-org/react";
+import unsplash from "./unsplashConfig";
+import { AcmeLogo } from "./AcmeLogo";
 
-export const RightListboxWrapper = () => {
+export const RightListboxWrapper = (props) => {
   const [values, setValues] = React.useState(new Set(["1"]));
-  const arrayValues = Array.from(values);
+
+  const [imageList, setImageList] = React.useState([]);
+
+  interface ImgContext {
+    url: string;
+    name: string;
+    avatar: string;
+    profile: string;
+    downloadLink: string;
+  }
+
+  const [unsplashImage, setUnsplashImage] = React.useState<ImgContext>({
+    url: "",
+    name: "",
+    avatar: "",
+    profile: "",
+    downloadLink: ""
+  });
 
 
-  const topContent = React.useMemo(() => {
-    if (!arrayValues.length) {
-      return null;
-    }
+  const searchImages = (searchText: string) => {
+    console.log('sssssssss')
+    unsplash.search
+      .getPhotos({
+        query: searchText,
+        page: 1,
+        per_page: 30,
+        // orientation:'portrait'
 
-    return (
-      <Navbar
-      classNames = {{
-        wrapper: "px-4",
-      }}
-      >
-      <NavbarBrand>
-        <p className="font-bold text-inherit">ACME</p>
-      </NavbarBrand>
-    
-      <NavbarContent justify="end">
-        <NavbarItem>
-          <Button as={Link} color="primary" href="#" variant="flat">
-            Sign Up
-          </Button>
-        </NavbarItem>
-      </NavbarContent>
-    </Navbar>
-    );
-  }, [arrayValues.length]);
+
+      })
+      .then(response => {
+        setImageList(response.response.results)
+      });
+  }
+
+   // 
+   const selectImage = (image) => {
+    props.onData({
+      url: image.urls.regular,
+      name: image.user.name,
+      avatar: image.user.profile_image.small,
+      profile: `${image.user.links.html}?utm_source=https://coverview.vercel.app&utm_medium=referral`,
+      downloadLink: image.links.download_location
+    })
+  }
+
 
   return (
-    <div className="w-full max-w-[w-80] flex flex-col h-screen bg-slate-800">
-      <div className="bg-gray-500 w-full">
-        {topContent}
-      </div>
-      <Listbox
-            className="flex-grow overflow-scroll"
-            items={users}
-            label="Assigned to"
-            selectionMode="none"
-            onSelectionChange={setValues}
-            variant="flat"
-            aria-label="Actions"
-            onAction={(key) => alert(key)}
-          >
-            {(item) => (
-              <ListboxItem key={item.id} textValue={item.name}>
-                <div className="flex gap-2 items-center">
-                  <Image
-                    className="w-full"
-                    alt="NextUI hero Image"
-                    src={item.avatar}
-                  />
-                </div>
-              </ListboxItem>
-            )}
-          </Listbox>
-          <div className="w-full">
-              {topContent}
+    <div className="w-full max-w-[w-80] h-screen bg-slate-800">
+      <Tabs fullWidth size="md" aria-label="Dynamic tabs" radius='sm'
+      >
+        <Tab key="property" title="属性">
+        </Tab>
+        <Tab key="photos" title="图库">
+          <div className="w-full flex flex-col bg-slate-800">
+            <div className="bg-gray-500 w-full">
+              <Navbar
+                classNames={{
+                  wrapper: "px-4",
+                }}
+              >
+                <NavbarBrand>
+                  <p className="font-bold text-inherit">ACME</p>
+                </NavbarBrand>
+
+                <NavbarContent justify="end">
+                  <NavbarItem>
+                    <Button onClick={() => searchImages("dog")}>
+                      <svg className="w-9 h-9 ml-auto mr-1 p-2 bg-gray-700 hover:bg-gray-800 text-white rounded-full" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                    </Button>
+                  </NavbarItem>
+                </NavbarContent>
+              </Navbar>
+            </div>
+            <Listbox
+              className="h-96 overflow-scroll"
+              items={imageList}
+              label="Assigned to"
+              selectionMode="none"
+              onSelectionChange={setValues}
+              variant="flat"
+              aria-label="Actions"
+            >
+              {(item) => (
+                <ListboxItem key={item.id} textValue={item.alt_description}>
+                  <div className="flex gap-2 items-center">
+                    <img src={item.urls.regular}
+                      key={item.id}
+                      alt={item.alt_description}
+                      className="rounded cursor-pointer w-full object-cover h-44"
+                      onClick={() => selectImage(item)
+                      }
+                    />
+
+                  </div>
+                </ListboxItem>
+              )}
+            </Listbox>
+
           </div>
+        </Tab>
+
+      </Tabs>
+
     </div>
   )
 };
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,19 +7,22 @@ import {users} from "./data";
 import {ImageEditor} from "./ImageEditor";
 
 export default function Home() {
-
-
+  const onChildData = (data:{}) => {
+    console.log('message from child ' + data.url);
+    setMessage(data)
+  }
+  const [message, setMessage] = React.useState('');
   return (
     <div className="flex">
        <div className="w-52 bg-gray-200">
         <ListboxWrapper>
         </ListboxWrapper>
       </div>
-      <div className="flex-1">
-        <ImageEditor/>
+      <div className="flex-1" >
+        <ImageEditor message={message}/>
       </div>
       <div className="w-80 bg-gray-320">
-        <RightListboxWrapper>
+        <RightListboxWrapper onData={onChildData}>
         </RightListboxWrapper>
       </div>
         
