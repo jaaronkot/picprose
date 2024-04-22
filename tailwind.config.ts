699a90a6173f9a95d540e1f1259c47b6c6653781
gezhaoyou <gezhaoyou@126.com>
2024-04-22 22:50:56 +0800

    rm  'src' dir

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,9 +4,9 @@ const { scrollbarGutter, scrollbarWidth, scrollbarColor } = require('tailwind-sc
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
